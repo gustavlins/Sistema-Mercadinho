b6d86998c1ba3865e252210c76401b67b6428c4c
Gustavo Lins <46906500+gustavlins@users.noreply.github.com>
2019-06-18 11:32:51 -0300

    Add files via upload

---
+++ SistemaMercadinhoGosling/src/model/bean/Produto.java
@@ -0,0 +1,72 @@
+package model.bean;
+
+
+public class Produto {
+    
+    private int id;
+    private String descricao;
+    private int qtd;
+    private double preco;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the qtd
+     */
+    public int getQtd() {
+        return qtd;
+    }
+
+    /**
+     * @param qtd the qtd to set
+     */
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    /**
+     * @return the preco
+     */
+    public double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return getDescricao(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ SistemaMercadinhoGosling/src/model/bean/Usuario.java
@@ -0,0 +1,54 @@
+
+package model.bean;
+
+
+public class Usuario {
+    private int id;
+    private String login;
+    private String senha;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
